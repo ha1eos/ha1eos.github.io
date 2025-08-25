206fac8693b75f6177c5f102d13c808c5eb13a2d
ha1eos <zachary.nguyen@jpschools.org>
2025-08-25 16:13:24 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(750,650,75,40, "lime");
+    createPlatform(525,575,75,40,'pink');
+    createPlatform(350,465,75,30,'blue');
+    createPlatform(200,400,75,30, 'red');
+    createPlatform(0,300,75,30, 'purple');
 
     // TODO 3 - Create Collectables
-
-
+    createCollectable("diamond",900,700,0.5,0.3);
+    createCollectable("steve", 400,250,0.8,0);
+    createCollectable("max",0,100,0.4,0);
+    createCollectable("kennedi",100,475,0,0);
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("left",500,3000);
+    createCannon("right",390,1500);
+    createCannon("right",450,1300);
     
     
     //////////////////////////////////
